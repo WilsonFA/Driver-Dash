69982e07c604ecb3a0276e3dc023f15ea995b1ff
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2025-08-26 10:41:52 -0300

    feat(driver): add new variables
    - Added speed and rotation variables
    - Prepared for player movement logic

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    float steerSpeed = 0.2f;
+    float moveSpeed = 0.02f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
