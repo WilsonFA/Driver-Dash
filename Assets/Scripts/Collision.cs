221ec8775fc96e3b4aec818b9850953864dc9f3c
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2025-09-15 11:00:21 -0300

    [feature] Add vehicle sprite, tiled ground, and new camera scene
    - Added vehicle sprite to player
    - Implemented tiled sprite for ground layout
    - Created a new scene to configure the camera with a custom setup

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Bateu!");
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         Debug.Log("Passou!");
     }
+
 }
