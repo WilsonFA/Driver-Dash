2dbba1c05bae6731e5d6f827bad8247c2a6093e5
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2025-10-28 11:21:23 -0300

    Add pickup delay and multiple package pickups
    Introduced a 'delayPickup' field to control the delay before destroying package objects after pickup. Updated the scene to add multiple package pickup prefabs and adjusted logic to prevent picking up a package when already carrying one.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,15 +1,18 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float delayPickup = 0f;
     bool hasPackage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up Package!");
             hasPackage = true;
+            Destroy(collision.gameObject, delayPickup);
         }
 
         if (collision.CompareTag("Customer") && hasPackage == true)
