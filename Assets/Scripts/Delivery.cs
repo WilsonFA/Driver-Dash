f62185812e5c1177cc9255c94428697c41c875b2
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2025-10-28 10:57:57 -0300

    Refactor package pickup to use prefab and add customer
    Replaced the in-scene package pickup object with a new prefab and updated the scene to instantiate it. Added a new 'Customer' tag and corresponding GameObject to the scene. Updated Delivery.cs to track package pickup and delivery to customers.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-
-    //if(Car collider package)
-    //then package destroy yourself and show message.
+    bool hasPackage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Package"))
         {
             Debug.Log("Picked up Package!");
+            hasPackage = true;
+        }
+
+        if (collision.CompareTag("Customer") && hasPackage == true)
+        {
+            Debug.Log("Delivered Package!");
+            hasPackage = false;
         }
     }
+
 }
